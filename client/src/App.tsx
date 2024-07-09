14a9da2c8c7f99687939fe970f38d69d67bab588
damianhobson <damian303@gmail.com>
2024-07-09 16:15:10 +0100

    Change title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Where is our Power coming from?</h1>
+        <h1>Electricity Generation Ireland</h1>
       </header>
       <section className="App-main"> 
         <GraphContainer title="Wind Generated"/>
